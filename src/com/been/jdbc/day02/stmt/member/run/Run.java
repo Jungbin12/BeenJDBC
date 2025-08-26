4243bc656926904eaa21e23e34d24b922883030f
user1 <user1@DESKTOP-HL31AHE>
2025-08-26 11:29:13 +0900

    [ADD] JDBC 프로그래밍 2일차 - 회원 관리 프로그램
    1. 회원 가입
    2. 회원 전체 정보 조회
    3. 회원 아이디로 검색
    4. 회원 정보 수정
    5. 회원 정보 삭제
    
    의 기능을 구현하였고 Statement 객체를 이용하여 쿼리문을 실행하고 그 결과 값을 활용하였다
    
    - SELECT 를 실행하고 나서는 ResultSet 으로 받아서 후처리를 해주어야 한다. DML 의 경우 int 로 받아서
    리턴해주어야 한다
    - return null 또는 return 0 으로 두어서 DB 에서 수정, 삭제는 되는데 메시지는 실패했다고 출력된 적이 있었다
    - 대부분의 오류가 쿼리문의 오타였다
    - 객체를 만들지 않거나 null 체크를 만해서 null 포인터 익셉션이 발생 할 수 있었다

---
+++ src/com/been/jdbc/day02/stmt/member/run/Run.java
@@ -0,0 +1,15 @@
+package com.been.jdbc.day02.stmt.member.run;
+
+
+import com.been.jdbc.day02.stmt.member.view.MemberView;
+
+
+public class Run {
+	public static void main(String[] args) {
+		MemberView view = new MemberView();
+		view.startProgram();
+		
+		
+			
+	}
+}
